47aa359f89215774de76b98cb8add5c29a6f8594
nakaya1182 <nakayakazuki1182@se-project.co.jp>
2019-10-31 11:45:32 +0900

    プロジェクト ファイルを追加します。

---
+++ marubatugame/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ButtonR = New System.Windows.Forms.Button()
+
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 200)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = ""
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(100, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = ""
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(200, 200)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = ""
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(0, 100)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = ""
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(100, 100)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = ""
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(200, 100)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 0
+        Me.Button6.Text = ""
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(0, 0)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 0
+        Me.Button7.Text = ""
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(100, 0)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = ""
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(200, 0)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 0
+        Me.Button9.Text = ""
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ButtonR
+        '
+        Me.ButtonR.Location = New System.Drawing.Point(350, 0)
+        Me.ButtonR.Name = "ButtonR"
+        Me.ButtonR.Size = New System.Drawing.Size(100, 100)
+        Me.ButtonR.TabIndex = 0
+        Me.ButtonR.Text = "リセット"
+        Me.ButtonR.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 300)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.ButtonR)
+        Me.Name = "Form1"
+        Me.Text = "〇×ゲーム"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonR As Button
+End Class
---
+++ marubatugame/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+    ' 整数2次元配列変数の初期化
+    Dim status(,) As Integer = {
+        {7, 8, 9},
+        {4, 5, 6},
+        {1, 2, 3}
+    }
+    Dim turn As Integer = TurnEnum.maru
+    Dim turnCount As Integer = 0
+    Public Enum TurnEnum
+        maru
+        batu
+    End Enum
+    Private Sub Button(button, num1, num2)
+        button.Font = New Font("ＭＳ Ｐゴシック", 50, FontStyle.Bold)
+        If status(num1, num2) < 10 Then
+            If turn = TurnEnum.maru Then
+                button.Text = "〇"
+                status(num1, num2) = 10
+                turn = TurnEnum.batu
+            Else
+                button.Text = "×"
+                status(num1, num2) = 20
+                turn = TurnEnum.maru
+            End If
+            turnCount += 1
+            Check()
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button(Button1, 2, 0)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button(Button2, 2, 1)
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button(Button3, 2, 2)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button(Button4, 1, 0)
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Button(Button5, 1, 1)
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Button(Button6, 1, 2)
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Button(Button7, 0, 0)
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Button(Button8, 0, 1)
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Button(Button9, 0, 2)
+    End Sub
+    Private Sub ButtonR_Click(sender As Object, e As EventArgs) Handles ButtonR.Click
+        Reset()
+    End Sub
+    Private Sub Check()
+        Dim i As Integer
+        For i = 0 To 2
+            If (status(i, 0) = status(i, 1) And status(i, 1) = status(i, 2)) Or
+                (status(0, i) = status(1, i) And status(1, i) = status(2, i)) Or
+                (status(0, 0) = status(1, 1) And status(1, 1) = status(2, 2)) Or
+                (status(0, 2) = status(1, 1) And status(1, 1) = status(2, 0)) Then
+                If turn = TurnEnum.batu Then
+                    MessageBox.Show("〇の勝ちデース",
+                    "終了",
+                    MessageBoxButtons.OK)
+                    Reset()
+                Else
+                    MessageBox.Show("×の勝ちデース",
+                    "終了",
+                    MessageBoxButtons.OK)
+                    Reset()
+                End If
+            End If
+        Next
+        If turnCount >= 9 Then
+            MessageBox.Show("引き分けです。",
+                    "終了",
+                    MessageBoxButtons.OK)
+            Reset()
+        End If
+    End Sub
+
+    Private Sub Reset()
+        turn = TurnEnum.maru
+        turnCount = 0
+        status = {
+        {7, 8, 9},
+        {4, 5, 6},
+        {1, 2, 3}
+    }
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+    End Sub
+End Class
+
